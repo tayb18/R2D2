88bd5a43bed47be82f56034f4c6944839f0bac5d
Basso Basso <taylorpbasso@gmail.com>
2015-11-05 13:26:53 -0500

    making slow progress.. have the general layout worked out. Getting started on the JS

---
+++ main.js
@@ -0,0 +1,17 @@
+var red = document.getElementByClassName('red');
+var blue = document.getElementByClassName('blue');
+var green = document.getElementByClassName('green');
+var yellow = document.getElementByClassName('yellow');
+var startButton = document.getElementByClassName('start');
+
+var pattern = [];
+
+var createPattern = function (){
+	
+}
+
+
+startButton.addEventListener('click', //call back function here);
+
+
+
